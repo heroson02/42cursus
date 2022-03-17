e4cd5cf18c62deb5528c298f6d806d9ce982bbc2
heroson02 <heroson0925@gmail.com>
2022-03-17 19:45:37 +0900

    new

---
+++ philosophers/philosophers/philosopher.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yson <yson@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/17 19:42:29 by yson              #+#    #+#             */
+/*   Updated: 2022/03/17 19:42:41 by yson             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_H
+# define PHILOSOPHER_H
+# include <pthread.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <stdio.h>
+
+#endif
