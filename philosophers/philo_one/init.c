a0de05ae885beb604e86c571b6a1bdb4144c1e39
heroson02 <heroson0925@gmail.com>
2022-03-18 23:57:36 +0900

    testing..

---
+++ philosophers/philo_one/init.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yson <yson@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 16:44:42 by yson              #+#    #+#             */
+/*   Updated: 2022/03/18 22:31:46 by yson             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher.h"
+
+int	arg_range_check(t_info *info, int argc)
+{
+	if (info->num_of_must_eat < 1 && argc == 6)
+		return (0);
+	if (info->num_of_must_eat < 0 || info->time_to_die < 0 \
+		|| info->time_to_eat < 0 || info->time_to_sleep < 0)
+		return (0);
+	return (1);
+}
+
+int	handle_arg(int argc, char **argv, t_info *info)
+{
+	memset(info, 0, sizeof(t_info));
+	info->num_of_philo = ft_atoi_ad(argv[1]);
+	info->time_to_die = ft_atoi_ad(argv[2]);
+	info->time_to_eat = ft_atoi_ad(argv[3]);
+	info->time_to_sleep = ft_atoi_ad(argv[4]);
+	if (argc == 6)
+		info->num_of_must_eat = ft_atoi_ad(argv[5]);
+	if (!arg_range_check(info, argc))
+		return (0);
+	return (1);
+}
+
+int	ft_malloc(void *dst, size_t size)
+{
+	*(void **)dst = malloc(size);
+	if (*(void **)dst == NULL)
+		return (0);
+	memset(*(void **)dst, 0, size);
+	return (1);
+}
+
+int malloc_arr(t_info *info)
+{
+	// info->philos = malloc(sizeof(t_philo) * info->num_of_philo);
+	// if (!info->philos)
+	// 	return (0);
+	// memset(&info->philos, 0, sizeof(t_philo) * info->num_of_philo);
+	// info->forks = malloc(sizeof(pthread_mutex_t) * info->num_of_philo);
+	// if (!info->forks)
+	// 	return (0);
+	// memset(&info->forks, 0, sizeof(pthread_mutex_t) * info->num_of_philo);
+	ft_malloc(&info->philos, sizeof(t_philo) * info->num_of_philo);
+	ft_malloc(&info->forks ,sizeof(pthread_mutex_t) * info->num_of_philo);
+	return (1);
+}
+
+int	init_philos(t_info *info)
+{
+	int i;
+
+	i = 0;
+	if (!malloc_arr(info))
+		return (0);
+	if (pthread_mutex_init(&info->finish_mutex, NULL))
+		return (0);
+	if (pthread_mutex_init(&info->print_mutex, NULL))
+		return (0);
+	while (i < info->num_of_philo)
+	{
+		info->philos[i].name = i;
+		info->philos[i].info = info;
+		if (pthread_mutex_init(&info->philos[i].check, NULL))
+			return (0);
+		if (pthread_mutex_init(&info->forks[i], NULL))
+			return (0);
+		info->philos[i].left = &info->forks[i];
+		if (i + 1 == info->num_of_philo)
+			info->philos[i].right = &info->forks[0];
+		else
+			info->philos[i].right = &info->forks[i + 1];
+		i++;
+	}
+	return (1);
+}
+
+int init(int argc, char **argv, t_info *info)
+{
+	if (argc != 5 && argc != 6)
+		return (0);
+	if (!handle_arg(argc, argv, info))
+		return (0);
+	if (!init_philos(info))
+		return (0);
+	return (1);
+}
--- philosophers/philo_one/utils.c
+++ philosophers/philo_one/utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yson <yson@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/18 18:06:14 by yson              #+#    #+#             */
-/*   Updated: 2022/03/18 23:43:50 by yson             ###   ########.fr       */
+/*   Created: 2022/03/18 23:46:06 by yson              #+#    #+#             */
+/*   Updated: 2022/03/18 23:46:13 by yson             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,43 +30,4 @@ void    print_mutex(t_philo *philo, char *str)
 		printf("%s\n", str);
 	}
     pthread_mutex_unlock(&philo->info->print_mutex);
-}
-
-void	join_and_free_philos(t_info *info)
-{
-	int		i;
-
-	i = 0;
-	while (i < info->num_of_philo)
-	{
-		pthread_join(info->philos[i].thread, NULL);
-		pthread_mutex_destroy(&info->philos[i++].check);
-	}
-	free(info->philos);
-	i = 0;
-	while (i < info->num_of_philo)
-		pthread_mutex_destroy(&info->forks[i++]);
-	free(info->forks);
-}
-
-void	create_philos(t_info *info)
-{
-	int			i;
-	pthread_t	thread;
-
-	info->start_time = get_time_ms();
-	i = 0;
-	while (i < info->num_of_philo)
-	{
-		info->philos[i].last_time_to_eat = info->start_time;
-		pthread_create(&info->philos[i].thread, NULL, philo, &info->philos[i]);
-		pthread_create(&thread, NULL, monitor, &info->philos[i]);
-		pthread_detach(thread);
-		++i;
-	}
-	if (info->num_of_must_eat != 0)
-	{
-		pthread_create(&thread, NULL, check_goal, info);
-		pthread_detach(thread);
-	}
 }
