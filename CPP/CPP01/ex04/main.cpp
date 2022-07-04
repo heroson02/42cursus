91d809a243424d0e743e82847b0307a68af6fada
Son_Hero <46701207+heroson02@users.noreply.github.com>
2022-07-04 17:22:24 +0900

    ex04

---
+++ CPP/CPP01/ex04/main.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int	main(int argc, char **argv)
+{
+	std::ifstream	input;
+	std::ofstream	output;
+	std::string		line;
+	size_t			nPos;
+
+	if (argc != 4)
+	{
+		std::cout << "Error : argument needs filename and two string." << std::endl; 
+		return (1);
+	}
+	if (input.fail() || output.fail())
+	{
+		std::cout << "Error : can't open file." << std::endl; 
+		return (1);
+	}
+	std::string		filename(argv[1]);
+	std::string		target(argv[2]);
+	std::string		replace(argv[3]);
+	input.open(filename);
+	output.open(filename + ".replace");
+	while (1)
+	{
+		std::getline(input, line);
+		while (1)
+		{
+			nPos = line.find(target, 0);
+			if (nPos == std::string::npos)
+			{
+				output << line;
+				break ;
+			}
+			else
+			{
+				output << line.substr(0, nPos) << replace;
+				line = line.substr(nPos + target.length());
+			}
+		}
+		if (input.eof())
+			break ;
+		output << std::endl;
+	}
+	input.close();
+	output.close();
+	return (0);
+}
