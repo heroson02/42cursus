f7d1edbb13d6e4bb852338acf2ab7feb37f8c15a
Son_Hero <heroson0925@gmail.com>
2022-10-07 20:21:48 +0900

    testing in VM

--- Inception/srcs/requirements/mariadb/tools/data.sql
+++ Inception/srcs/requirements/mariadb/tools/data.sql
@@ -1,6 +1,5 @@
-CREATE DATABASE Data; --DB 생성하기
-CREATE USER 'yson'@'localhost' IDENTIFIED BY 'password'; --유저 생성하기
-GRANT ALL PREVILEGES ON Data.* TO 'yson'@'localhost'; --사용자 권한 설정
-ALTER USER 'root'@'localhost' IDENTIFIED BY '0000'; --루트 비밀번호 변경
-FLUSH PRIVILEGES; --변경사항 적용
-exit;
+CREATE DATABASE Data;
+CREATE USER '$NAME'@'%' IDENTIFIED BY '$PWD';
+GRANT ALL PRIVILEGES ON Data.* TO '$NAME'@'%' IDENTIFIED BY '$PWD';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOTPWD';
+FLUSH PRIVILEGES;
---
+++ Inception/srcs/requirements/wordpress/tools/wp-config.php
@@ -0,0 +1,40 @@
+<?php
+
+define( 'DB_NAME', getenv('DATABASE') );
+
+/** Database username */
+define( 'DB_USER', getenv('NAME') );
+
+/** Database password */
+define( 'DB_PASSWORD', getenv('PWD') );
+
+/** Database hostname */
+define( 'DB_HOST', 'mariadb' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
