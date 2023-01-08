26c43b85150722aeeddf488d3b5395a374bbf276
Son_Hero <heroson0925@gmail.com>
2023-01-09 01:14:32 +0900

    exam rank 05 공부 중

---
+++ exam/exam_rank_05/cpp_module_00/Warlock.hpp
@@ -0,0 +1,20 @@
+#ifndef WARLOCK_HPP
+#define WARLOCK_HPP
+
+#include <iostream>
+
+class Warlock
+{
+private:
+	std::string name;
+	std::string title;
+public:
+	Warlock(std::string _name, std::string _title);
+	~Warlock();
+	const std::string &getName() const;
+	const std::string &getTitle() const;
+	void	setTitle(const std::string a);
+	void	introduce() const;
+};
+
+#endif
